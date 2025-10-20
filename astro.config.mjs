27bd98dab2324b60c08cc391be29132a0d0dce21
BraynSramos <braynramos97@gmail.com>
2025-10-20 14:59:41 -0500

    sin rutas ✖️

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,7 @@ import react from "@astrojs/react";//Dependecia de React-👾
 // https://astro.build/config
 export default defineConfig({
     // Cambia USERNAME y REPO_NAME por los tuyos
-  site: 'https://brayanSramos.github.io/Mi-Porfolio',
-  base: '/Mi-Porfolio/',    // importante para que los assets y rutas usen /REPO_NAME/
+  site: 'https://brayanSramos.github.io/Mi-Porfolio',    // importante para que los assets y rutas usen /REPO_NAME/
   output: 'static',
   integrations: [tailwind(), //Configuracion de Tailwind-👾
   react(),

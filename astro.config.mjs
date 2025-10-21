e6861ce7afff3439191eb107b14ad5d21a37a8db
BraynSramos <42010469+brayanSramos@users.noreply.github.com>
2025-10-21 15:31:17 -0500

    Corregir la URL del sitio en la configuración de Astro para asegurar rutas limpias 📎

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import react from "@astrojs/react";//Dependecia de React-👾
 // https://astro.build/config
 export default defineConfig({
     // Cambia USERNAME y REPO_NAME por los tuyos
-  site: 'https://brayanSramos.github.io',
+  site: 'https://brayanSramos.github.io/',
   base: '/', // Asegura rutas limpias (sin /Mi-Porfolio/)
   output: 'static',
   integrations: [tailwind(), //Configuracion de Tailwind-👾

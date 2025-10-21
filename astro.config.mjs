591270291b4e1c93d3860b5313de264382137864
BraynSramos <braynramos97@gmail.com>
2025-10-21 16:12:44 -0500

    Reorganizar comentario en la configuración de Astro para mayor claridad

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,7 @@ import react from "@astrojs/react";//Dependecia de React-👾
 export default defineConfig({
     // Cambia USERNAME y REPO_NAME por los tuyos
   site: 'https://brayanSramos.github.io/',
-  base: '/', 
-  // Asegura rutas limpias (sin /Mi-Porfolio/)
+  base: '/', // Asegura rutas limpias (sin /Mi-Porfolio/)
   output: 'static',
   integrations: [tailwind(), //Configuracion de Tailwind-👾
   react(),
